447a410ebede2b847bf4021cc3640e92d044807d
Gizzat <gizzat@gmail.com>
2020-04-14 14:43:43 +0600

    Some improvements

--- drkiq/app/controllers/pages_controller.rb
+++ drkiq/app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def home
     @meaning_of_life = CounterJob.perform_now
   end
+
+  def article
+    @article = Article.first
+  end
 end
--- drkiq/config/routes.rb
+++ drkiq/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get '/pages/article', 'pages#article'
 end
